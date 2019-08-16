eb5f75f826a2a667100169d677707dad48def900
Colin Amos <35937916+ColinAmos@users.noreply.github.com>
2019-08-16 17:09:12 -0400

    Program SearchBar
    Create and style search bar.
    Hook up event handlers to handle input change and submit.
    Get youtube api response in submit handler.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,48 @@ import youtube from "./api/youtube";
 
 import { SearchBar, VideoDetails, VideoList } from "./components";
 
-const styles = {
-    backgroundColor: "red",
-    border: "2px black dotted"
+const styleGridItem = {
+    //backgroundColor: "",
+    //border: "2px black dotted"
 }
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null,
+        }
+    }
+
+    handleSearchSubmit = async (searchTerm) => {
+        const response = await youtube.get("search", {
+            params: {
+                part: "snippet",
+                maxResults: 5,
+                key: "AIzaSyDRA6dw4qaOe5UxcX4J8NQ3fv_WhYpTrCg",
+                q: searchTerm,
+            }
+        });
+
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0],
+        });
+    }
+
     render() {
         return (
             <Grid container justify="center" spacing={2}>
                 <Grid item xs={12}>
                     <Grid container spacing={2}>
-                        <Grid item xs={12} style={styles}>
-                            <SearchBar />
+                        <Grid item xs={12} style={styleGridItem}>
+                            <SearchBar onFormSubmit={this.handleSearchSubmit}/>
                         </Grid>
-                        <Grid item xs={8} style={styles}>
+                        <Grid item xs={8} style={styleGridItem}>
                             <VideoDetails />
                         </Grid>
-                        <Grid item xs={4} style={styles}>
+                        <Grid item xs={4} style={styleGridItem}>
                             <VideoList />
                         </Grid>
                     </Grid>
